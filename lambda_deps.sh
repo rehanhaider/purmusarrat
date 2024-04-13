8ba9497e4ba40b620c4bf960cebf94d058e3b14f
Rehan Haider <email@rehanhaider.com>
2024-04-13 11:41:48 +0530

    FEAT: created scripts to setup python dev environment easily

--- lambda_deps.sh
+++ lambda_deps.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-separator="--------------------------------------------------------------------------------------------------------------------------"
-
-
 activate_python_virtual_env() {
     # Activate the python virtual environment, if it exists
     # If it doesn't exist, create it
@@ -19,72 +16,20 @@ activate_python_virtual_env() {
 }
 
 
-file_mod() {
-    file="$1"
-    status=$(git status --porcelain "$file" 2>/dev/null)
-    if [ -n "$status" ]; then
-        return 0  # File modified
-    else
-        return 1  # File not modified
-    fi
-}
-
-default() {
-    #activate_python_virtual_env
-    find src -name "requirements.in" -type f | while read -r file; do
-        echo "Requirements unchanged: $file"
-        if file_mod "$file"; then
-            echo "File modified: $file"
-            echo "Updating requirements.txt"
-            pip-compile -q --strip-extras "$file"
-        fi
-        # check if the generated requirements.txt is different from the one in git
-        if file_mod "${file%.*}.txt"; then
-            echo "File modified: ${file%.*}.txt"
-            pip-sync -q "${file%.*}.txt"
-        fi
+create_layer() {
+    # Create a lambda layer
+    activate_python_virtual_env
+    rm -rf layer_python
+    echo "Creating lambda layer"
+    find src -name "requirements.txt" -type f | while read -r file; do
+        echo "Installing requirements.txt for: $file"
+        mkdir -p layer_python
+        python3.11 -m pip install -r $file -t layer_python
     done
+    echo "Lambda layer created"
 }
-
-
 
 if [ $# -eq 0 ]; then
-    default
+    create_layer
     exit 0
 fi
-
-force_update() {
-    # search for all requirements.txt files
-    find src -name "requirements.in" -type f | while read -r file; do
-        echo "Deleting requirements.txt for: $file"
-        rm "${file%.*}.txt"
-        echo "Updating requirements.txt"
-        pip-compile -q --strip-extras "$file"
-        echo "Installing requirements.txt"
-        pip-sync -q "${file%.*}.txt"
-    done
-}
-
-
-
-
-while getopts "fh" OPTION; do
-    case $OPTION in
-        f)
-            force_update
-            exit 0
-            ;;
-        h)
-            echo "Usage: $0 [-f] [-h]"
-            echo "  -f: Force update of requirements.txt"
-            echo "  -h: Show this help message"
-            exit 0
-            ;;
-        *)
-            echo "Usage: $0 [-f] [-h]"
-            echo "  -f: Force update of requirements.txt"
-            echo "  -h: Show this help message"
-            exit 1
-            ;;
-    esac
-done
