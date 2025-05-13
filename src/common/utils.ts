19983ff9f32d6224a8dccafc1b9efddbbacf0dd6
Rehan Haider <email@rehanhaider.com>
2025-05-13 18:47:19 +0000

    FEAT:  cleaned dir

--- src/common/utils.ts
+++
@@ -1,6 +0,0 @@
-export function getJsonData(FilePath: string): Record<string, any> {
-    const fs = require('fs');
-    const JsonFile = fs.readFileSync(FilePath, 'utf8');
-    const JsonData = JSON.parse(JsonFile);
-    return JsonData;
-}
